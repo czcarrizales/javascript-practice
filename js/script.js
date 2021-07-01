afac7e090c3728f53f64d82941b65cf657c7696c
czcarrizales <czcarrizales@yahoo.com>
2021-07-01 13:42:31 -0700

    Added comments, console.logs, and if/else function

---
+++ js/script.js
@@ -0,0 +1,30 @@
+//Logging name to the console
+console.log('Chazz');
+
+/*
+Logging my
+FAVORITE
+food to the
+CONSOLE
+*/
+console.log('Pepperoni Pizza');
+
+//Alert when button is pressed
+function buttonAlert(clicked_id) {
+    if (clicked_id == 1) {
+        alert('Chazz Carrizales');
+    }
+    else if (clicked_id == 2) {
+        alert('Pepperoni Pizza');
+    }
+    else if (clicked_id == 3) {
+        alert('Whiplash');
+    }
+    else if (clicked_id == 4) {
+        alert('Blue');
+    }
+    else if (clicked_id == 5) {
+        alert('Wolf');
+    }
+}
+
